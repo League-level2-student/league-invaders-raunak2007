4f9129e67a085a22047c4db3534724965f701004
League Student <league@WTS14-3.attlocal.net>
2020-02-22 14:29:32 -0800

    league snake,league invaders
    :smile:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH=500;
+	public static final int HEIGHT=800;
+	LeagueInvaders(JFrame f){
+		this.frame=f;
+	}
+	void setup() {
+		size(WIDTH,HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
---
+++ src/LeagueInvadersRunner.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class LeagueInvadersRunner {
+	
+	public static void main(String[] args) {
+		JFrame frame=new JFrame();
+		new LeagueInvaders(frame).setup();
+	}
+}
